d58bbf5386583b347edfc302a6aaa41039cce035
zopapami <zopapami@gmail.com>
2023-01-14 02:44:12 +0200

    setup controller

---
+++ back-end/app/controllers/movie.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Movie = db.movies;
+
+// create and Save a new Movie
+exports.create = (req, res) => {
+  
+};
+
+// retrieve all Movies from the database
+exports.findAll = (req, res) => {
+  
+};
+
+// find a single Movie with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// update a Movie by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// delete a Movie with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// delete all Movies from the database
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all watched Movies
+exports.findAllPublished = (req, res) => {
+  
+};
--- back-end/app/models/movie.model.js
+++ back-end/app/models/movie.model.js
@@ -1,14 +1,19 @@
 module.exports = mongoose => {
-  const Movie = mongoose.model(
-    "movie",
-    mongoose.Schema(
-      {
-        title: String,
-        year: Number,
-        watched: Boolean
-      },
-      { timestamps: true }
-    )
+  let schema = mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      watched: Boolean
+    },
+    { timestamps: true }
   );
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Movie = mongoose.model("movie", schema);
   return Movie;
 };
